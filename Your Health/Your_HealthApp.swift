f81efc78aa2e1b9c4eaa90ecb46988fc530cbd46
Maimai10808 <331686549@qq.com>
2025-03-14 18:38:35 +0800

    Initial Commit

---
+++ Your Health/Your_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_HealthApp.swift
+//  Your Health
+//
+//  Created by mac on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Your_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
